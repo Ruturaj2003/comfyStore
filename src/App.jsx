eb2dc5c5e648e933b9dff36170c94ef8d1639256
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-04-07 13:56:32 +0530

    Setup Starting Project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-300 text-3xl">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
