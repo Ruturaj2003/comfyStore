45f564f6a7c461eb80223f79d2fa2376a5c27208
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-11 11:33:01 +0530

    Implement NavBar Structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
+import { FaBarsStaggered } from 'react-icons/fa6';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar align-elements">
+        <div className="navbar-start">
+          {/* Title */}
+          <NavLink
+            to={'/'}
+            className="hidden lg:flex btn btn-primary text-3xl items-center font-serif"
+          >
+            C
+          </NavLink>
+          {/* DropDown */}
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="h-6 w-6"></FaBarsStaggered>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-300"
+            >
+              Nav links
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">Nav Links</ul>
+        </div>
+        <div className="navbar-end">
+          {/* Theme Setup */}
+          {/* Cart Link */}
+          <NavLink
+            to={'/cart'}
+            className={'btn btn-ghost btn-circle btn-md ml-4'}
+          >
+            <div className="indicator">
+              <BsCart3 className="h-6 w-6"></BsCart3>
+              <span className="badge badge-neutral indicator-item">69</span>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
