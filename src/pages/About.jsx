55e1f34bc2b84a847bd4f789ee8a574db057e7d4
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-13 19:24:14 +0530

    Finish About pAGE

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,27 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We Love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg  leading-8 max-w-2xl mx-auto">
+        Far far away, behind the word mountains, far from the countries Vokalia
+        and Consonantia, there live the blind texts. Separated they live in
+        Bookmarksgrove right at the coast of the Semantics, a large language
+        ocean. A small river named Duden flows by their place and supplies it
+        with the necessary regelialia. It is a paradisematic country, in which
+        roasted parts of sentences fly into your mouth.
+      </p>
+    </>
+  );
 };
 export default About;
