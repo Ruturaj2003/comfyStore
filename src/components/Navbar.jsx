8f85d26d8dc25ef6227872dab7c861159e8343c9
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-13 19:16:21 +0530

    Implement Theme Setting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,29 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const themes = {
+  garden: 'garden',
+  forest: 'forest',
+};
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem('theme') || themes.forest;
+};
 
 const Navbar = () => {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(getThemeFromLocalStorage);
   const handleTheme = () => {
-    setTheme(!theme);
+    const { garden, forest } = themes;
+    const newTheme = theme === garden ? forest : garden;
+
+    setTheme(newTheme);
   };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
     <nav>
       <div className="navbar align-elements">
