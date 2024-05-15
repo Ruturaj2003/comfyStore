1afbb39a61acb4d4782131c9b80f8cacd5b8fcac
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-15 19:05:44 +0530

    Implement Fetching of Single Product Data

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import { ErrorElement } from './components';
 
 // Loaders
 import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
 
 // Actions
 
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        loader: singleProductLoader,
       },
       { path: 'cart', element: <Cart /> },
       { path: 'checkout', element: <Checkout /> },
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,23 @@
+import { Link, useLoaderData } from 'react-router-dom';
+import { customFetch, formatPrice } from '../utils';
+import { useState } from 'react';
+
+export const loader = async ({ params }) => {
+  const url = '/products/';
+  const id = params.id;
+  const response = await customFetch(url + id);
+
+  return {
+    product: response.data.data,
+  };
+};
+
 const SingleProduct = () => {
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const inrAmount = formatPrice(price);
+
   return <div>SingleProduct</div>;
 };
 export default SingleProduct;
