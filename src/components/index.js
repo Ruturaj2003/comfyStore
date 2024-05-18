80c5614d579d60dfacf0d3aa44b6e0a0496e4c41
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-18 19:21:55 +0530

    Update Cart Page UI

--- src/components/index.js
+++ src/components/index.js
@@ -12,3 +12,5 @@ export { default as ProductsList } from './ProductsList';
 export { default as Filters } from './Filters';
 export { default as ProductsContainer } from './ProductsContainer';
 export { default as PaginationContainer } from './PaginationContainer';
+export { default as CartItemsList } from './CartItemsList';
+export { default as CartTotals } from './CartTotals';
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,36 @@
+import { useSelector } from 'react-redux';
+import { CartItemsList, CartTotals, SectionTitle } from '../components';
+import { Link } from 'react-router-dom';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const user = null;
+  const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+
+  if (numItemsInCart === 0)
+    <SectionTitle text={'Your Cart is Empty'}></SectionTitle>;
+
+  return (
+    <>
+      <SectionTitle text={'Shopping Cart'}></SectionTitle>
+
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList></CartItemsList>
+        </div>
+        <div className="lg:col-span-4">
+          <CartTotals></CartTotals>
+          {user ? (
+            <Link to={'/checkout'} className="btn btn-primary btn-block mt-8 ">
+              Proceed to Checkout
+            </Link>
+          ) : (
+            <Link to={'/login'} className="btn btn-primary btn-block mt-8">
+              Please Log In
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 export default Cart;
