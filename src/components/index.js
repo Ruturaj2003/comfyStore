e13bc50c563f95c1ea53705a16228b97fb76dfd6
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-21 10:58:04 +0530

    Create Checkout Component

--- src/components/index.js
+++ src/components/index.js
@@ -14,3 +14,4 @@ export { default as ProductsContainer } from './ProductsContainer';
 export { default as PaginationContainer } from './PaginationContainer';
 export { default as CartItemsList } from './CartItemsList';
 export { default as CartTotals } from './CartTotals';
+export { default as CheckoutForm } from './CheckoutForm';
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,20 @@
+import { useSelector } from 'react-redux';
+import { CheckoutForm, SectionTitle, CartTotals } from '../components';
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const cartItems = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotal.length === 0) {
+    return <SectionTitle text={'Your Cart is Empty '}></SectionTitle>;
+  }
+
+  return (
+    <>
+      <SectionTitle text={'Place your order'}></SectionTitle>
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm></CheckoutForm>
+        <CartTotals></CartTotals>
+      </div>
+    </>
+  );
 };
 export default Checkout;
