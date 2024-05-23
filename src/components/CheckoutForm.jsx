f8b66950f7a961b8df4596efadb33e846a31ba97
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-05-23 19:51:14 +0530

    Finish Project

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 import { clearCart } from '../features/cart/cartSlice';
 
 export const action =
-  (store) =>
+  (store, queryClient) =>
   async ({ request }) => {
     const formData = await request.formData();
     const { name, address } = Object.fromEntries(formData);
@@ -34,6 +34,8 @@ export const action =
           },
         }
       );
+      queryClient.removeQueries(['orders']);
+
       store.dispatch(clearCart());
       toast.success('Order Placed');
       return redirect('/orders');
